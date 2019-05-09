8ec15170c98d364926df3fb75af8f8068f4299e6
SensorsIOT <andreas.spiess@arumba.com>
2019-05-09 16:01:00 +0200

    Initial Commit

---
+++ Tracker-lowpower-abp/Tracker-lowpower-abp.ino
@@ -0,0 +1,289 @@
+/*******************************************************************************
+   Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+
+   Permission is hereby granted, free of charge, to anyone
+   obtaining a copy of this document and accompanying files,
+   to do whatever they want with them without any restriction,
+   including, but not limited to, copying, modification and redistribution.
+   NO WARRANTY OF ANY KIND IS PROVIDED.
+
+   This example sends a valid LoRaWAN packet with payload "Hello,
+   world!", using frequency and encryption settings matching those of
+   the The Things Network.
+
+   This uses ABP (Activation-by-personalisation), where a DevAddr and
+   Session keys are preconfigured (unlike OTAA, where a DevEUI and
+   application key is configured, while the DevAddr and session keys are
+   assigned/generated in the over-the-air-activation procedure).
+
+   Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
+   g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
+   violated by this sketch when left running for longer)!
+
+   To use this sketch, first register your application and device with
+   the things network, to set or generate a DevAddr, NwkSKey and
+   AppSKey. Each device should have their own unique values for these
+   fields.
+
+   Do not forget to define the radio type correctly in config.h.
+
+ *******************************************************************************/
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+
+
+#define SLEEP_ESP32
+
+
+// LoRaWAN NwkSKey, network session key
+// msb
+static const PROGMEM u1_t NWKSKEY[16] = { 0x04, 0xD0, 0x0F, 0x2A, 0x2B, 0x20, 0xF5, 0x4F, 0xB0, 0x67, 0xFC, 0xBB, 0x41, 0x51, 0x21, 0x0D };
+
+// LoRaWAN AppSKey, application session key
+// lsb
+static const u1_t PROGMEM APPSKEY[16] = { 0xA2, 0xFD, 0x45, 0xD3, 0x1B, 0x85, 0xE0, 0x6D, 0x44, 0xCC, 0xFB, 0x23, 0xBC, 0x1E, 0x90, 0x47 };
+
+// LoRaWAN end-device address (DevAddr)
+static const u4_t DEVADDR = 0x26011E67 ; // <-- Change this address for every node!
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+static uint8_t mydata[] = "Hello, world!";
+static osjob_t sendjob;
+bool next = false;
+unsigned long entry;
+
+uint8_t txBuffer[9];
+uint32_t LatitudeBinary, LongitudeBinary;
+uint16_t altitudeGps;
+uint8_t hdopGps;
+String toLog;
+uint8_t coords[9];
+
+float FIX_LAT = 44.163761;
+float FIX_LONG = 28.646676;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 1800;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+  .nss = 18,
+  .rxtx = LMIC_UNUSED_PIN,
+  .rst = LMIC_UNUSED_PIN,
+  .dio = {/*dio0*/ 26, /*dio1*/ 33, /*dio2*/ 32}
+};
+
+
+void get_coords()
+{
+  LatitudeBinary = ((FIX_LAT + 90) / 180.0) * 16777215;
+  LongitudeBinary = ((FIX_LONG + 180) / 360.0) * 16777215;
+
+  txBuffer[0] = ( LatitudeBinary >> 16 ) & 0xFF;
+  txBuffer[1] = ( LatitudeBinary >> 8 ) & 0xFF;
+  txBuffer[2] = LatitudeBinary & 0xFF;
+
+  txBuffer[3] = ( LongitudeBinary >> 16 ) & 0xFF;
+  txBuffer[4] = ( LongitudeBinary >> 8 ) & 0xFF;
+  txBuffer[5] = LongitudeBinary & 0xFF;
+
+  altitudeGps = 350;
+  txBuffer[6] = ( altitudeGps >> 8 ) & 0xFF;
+  txBuffer[7] = altitudeGps & 0xFF;
+
+  hdopGps = 40 / 10;
+  txBuffer[8] = hdopGps & 0xFF;
+
+  Serial.println(LatitudeBinary, HEX);
+  Serial.println(LongitudeBinary, HEX);
+
+}
+
+void onEvent (ev_t ev) {
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch (ev) {
+    case EV_SCAN_TIMEOUT:
+      Serial.println(F("EV_SCAN_TIMEOUT"));
+      break;
+    case EV_BEACON_FOUND:
+      Serial.println(F("EV_BEACON_FOUND"));
+      break;
+    case EV_BEACON_MISSED:
+      Serial.println(F("EV_BEACON_MISSED"));
+      break;
+    case EV_BEACON_TRACKED:
+      Serial.println(F("EV_BEACON_TRACKED"));
+      break;
+    case EV_JOINING:
+      Serial.println(F("EV_JOINING"));
+      break;
+    case EV_JOINED:
+      Serial.println(F("EV_JOINED"));
+      break;
+    case EV_RFU1:
+      Serial.println(F("EV_RFU1"));
+      break;
+    case EV_JOIN_FAILED:
+      Serial.println(F("EV_JOIN_FAILED"));
+      break;
+    case EV_REJOIN_FAILED:
+      Serial.println(F("EV_REJOIN_FAILED"));
+      break;
+    case EV_TXCOMPLETE:
+      Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+      if (LMIC.txrxFlags & TXRX_ACK)
+        Serial.println(F("Received ack"));
+      if (LMIC.dataLen) {
+        Serial.println(F("Received "));
+        Serial.println(LMIC.dataLen);
+        Serial.println(F(" bytes of payload"));
+      }
+      // Schedule next transmission
+      os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+      next = true;
+      break;
+    case EV_LOST_TSYNC:
+      Serial.println(F("EV_LOST_TSYNC"));
+      break;
+    case EV_RESET:
+      Serial.println(F("EV_RESET"));
+      break;
+    case EV_RXCOMPLETE:
+      // data received in ping slot
+      Serial.println(F("EV_RXCOMPLETE"));
+      break;
+    case EV_LINK_DEAD:
+      Serial.println(F("EV_LINK_DEAD"));
+      break;
+    case EV_LINK_ALIVE:
+      Serial.println(F("EV_LINK_ALIVE"));
+      break;
+    default:
+      Serial.println(F("Unknown event"));
+      break;
+  }
+}
+
+void do_send(osjob_t* j) {
+  // Check if there is not a current TX/RX job running
+
+  if (LMIC.opmode & OP_TXRXPEND) {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  } else {
+    // Prepare upstream data transmission at the next possible time.
+    get_coords();
+    LMIC_setTxData2(1, (uint8_t*) txBuffer, sizeof(txBuffer), 0);
+    Serial.println(F("Packet queued"));
+  }
+  // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println(F("Starting"));
+
+#ifdef SLEEP_ESP32
+  esp_sleep_enable_timer_wakeup(TX_INTERVAL * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TX_INTERVAL) + " Seconds");
+#endif
+
+#ifdef VCC_ENABLE
+  // For Pinoccio Scout boards
+  pinMode(VCC_ENABLE, OUTPUT);
+  digitalWrite(VCC_ENABLE, HIGH);
+  delay(1000);
+#endif
+
+  // LMIC init
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+
+  // Set static session parameters. Instead of dynamically establishing a session
+  // by joining the network, precomputed session parameters are be provided.
+#ifdef PROGMEM
+  // On AVR, these values are stored in flash and only copied to RAM
+  // once. Copy them to a temporary buffer here, LMIC_setSession will
+  // copy them into a buffer of its own again.
+  uint8_t appskey[sizeof(APPSKEY)];
+  uint8_t nwkskey[sizeof(NWKSKEY)];
+  memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+  memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+  LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+#else
+  // If not running an AVR with PROGMEM, just use the arrays directly
+  LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+#endif
+
+#if defined(CFG_eu868)
+  // Set up the channels used by the Things Network, which corresponds
+  // to the defaults of most gateways. Without this, only three base
+  // channels from the LoRaWAN specification are used, which certainly
+  // works, so it is good for debugging, but can overload those
+  // frequencies, so be sure to configure the full frequency range of
+  // your network here (unless your network autoconfigures them).
+  // Setting up channels should happen after LMIC_setSession, as that
+  // configures the minimal channel set.
+  // NA-US channels 0-71 are configured automatically
+  LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+  LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+  // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+  // devices' ping slots. LMIC does not have an easy way to define set this
+  // frequency and support for class B is spotty and untested, so this
+  // frequency is not configured here.
+#elif defined(CFG_us915)
+  // NA-US channels 0-71 are configured automatically
+  // but only one group of 8 should (a subband) should be active
+  // TTN recommends the second sub band, 1 in a zero based count.
+  // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+  LMIC_selectSubBand(1);
+#endif
+
+  // Disable link check validation
+  LMIC_setLinkCheckMode(0);
+
+  // TTN uses SF9 for its RX2 window.
+  LMIC.dn2Dr = DR_SF9;
+
+  // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+  LMIC_setDrTxpow(DR_SF7, 15);
+
+  // Start job
+  do_send(&sendjob);
+
+#ifdef SLEEP_ESP32
+  Serial.println("Going to send");
+  entry = millis();
+  while (!next || (millis() - entry) > 300000) {
+    os_runloop_once();
+  }
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush();
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
+#endif
+}
+
+void loop() {
+  os_runloop_once();  // Will never run if SLEEP_ESP is set
+}
